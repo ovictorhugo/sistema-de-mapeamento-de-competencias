47317221ca17a52184eadca38ec9a9b1cc3c81fa
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2024-01-11 12:26:48 -0300

    45

--- src/components/ChatContent.tsx
+++ src/components/ChatContent.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Header } from './Header';
-import { Funnel, MagnifyingGlass, PaperPlaneTilt } from 'phosphor-react';
+import { Books, Copyright, Funnel, IdentificationCard, Lightbulb, MagnifyingGlass, PaperPlaneTilt, Plus, Quotes, TextAlignLeft, Ticket, Trash, X } from 'phosphor-react';
 import compromise from 'compromise';
 import nlp from 'compromise'
 import { UserContext } from '../contexts/context';
@@ -56,6 +56,7 @@ export function ChatContent() {
   const [inputValue, setInputValue] = useState<string>('');
   const [result, setResult] = useState<{ term: string; type: ResourceType } | null>(null);
   const { user, setUser } = useContext(UserContext);
+  const [isTab, setIsTab] = useState(false);
 
   const [messages, setMessages] = useState<Maria[]>([]);
   const { botaoPatentesClicado, setBotaoPatentesClicado } = useContext(UserContext);
@@ -256,12 +257,124 @@ export function ChatContent() {
   }
 
   console.log(`messages`, messages)
+
+   //estado btns 
+   const [selectedTab, setSelectedTab] = useState(0);
+
+   //Se o botão Pesquisadores for clicado
+   const handleClickPesquisadores = () => {
+     setBotaoPesquisadoresClicado(true);
+     setBotaoPatentesClicado(false)
+     setBotaoTermosClicado(false);
+     setBotaoAreasClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(false)
+     //Apagar checkbox ao mudar de aba - termos
+
+     setSelectedTab(3);
+   };
+
+   //patentes
+   const handleClickPatentes = () => {
+     setBotaoPesquisadoresClicado(false);
+     setBotaoPatentesClicado(true)
+     setBotaoTermosClicado(false);
+     setBotaoAreasClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(false)
+     //Apagar checkbox ao mudar de aba - termos
+
+     setSelectedTab(4);
+   };
+
+   //Se o botão Termos for clicado
+   const handleClickTermos = () => {
+     setBotaoPesquisadoresClicado(false);
+     setBotaoPatentesClicado(false)
+     setBotaoTermosClicado(true);
+     setBotaoAreasClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(false)
+
+     setSelectedTab(0);
+   };
+
+   const handleClickResumo = () => {
+     setBotaoPesquisadoresClicado(false);
+     setBotaoResumoClicado(true);
+     setBotaoPatentesClicado(false)
+     setBotaoAreasClicado(false);
+     setBotaoTermosClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(false)
+     //Apagar checkbox ao mudar de aba - pesqisadores
+
+     setSelectedTab(1);
+
+   };
+
+   //Se o botão Areas for clicado
+   const handleClickAreas = () => {
+     setBotaoAreasClicado(true);
+     setBotaoPesquisadoresClicado(false);
+     setBotaoPatentesClicado(false)
+     setBotaoTermosClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(false)
+     //Apagar checkbox ao mudar de aba - pesqisadores
+
+     setSelectedTab(2);
+   };
+
+   //Se o botão Livros for clicado
+   const handleClickLivrosCapitulos = () => {
+     setBotaoAreasClicado(false);
+     setBotaoPesquisadoresClicado(false);
+     setBotaoPatentesClicado(false)
+     setBotaoTermosClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(false)
+     setBotaoLivrosCapitulosClicado(true)
+     //Apagar checkbox ao mudar de aba - pesqisadores
+
+     setSelectedTab(5);
+   };
+
+   const handleClickEventos = () => {
+     setBotaoAreasClicado(false);
+     setBotaoPesquisadoresClicado(false);
+     setBotaoPatentesClicado(false)
+     setBotaoTermosClicado(false);
+     setBotaoResumoClicado(false)
+     setBotaoEventosClicado(true)
+     setBotaoLivrosCapitulosClicado(false)
+     //Apagar checkbox ao mudar de aba - pesqisadores
+
+     setSelectedTab(6);
+   };
+
+   const handleClickClose = () => {
+    setBotaoAreasClicado(false);
+    setBotaoPesquisadoresClicado(false);
+    setBotaoPatentesClicado(false)
+    setBotaoTermosClicado(true);
+    setBotaoResumoClicado(false)
+    setBotaoEventosClicado(false)
+    setBotaoLivrosCapitulosClicado(false)
+    //Apagar checkbox ao mudar de aba - pesqisadores
+    setIsTab(!isTab)
+    setSelectedTab(0);
+  };
  
   return (
     <div>
      <Header/>
 
-     <div className='pt-24 px-16 flex'>
+     <div className='pt-24 px-16 flex h-screen'>
 
 <div className='h-full w-[350px] border-r border-r-gray-300'>
   fgb
@@ -271,7 +384,7 @@ export function ChatContent() {
       <div>
 
 
-      <div className='flex flex-col'>
+      <div className='flex flex-col w-full pl-16'>
         {messages.map((message, i) => {
           if (message.role === '|assistant') {
             return (
@@ -303,7 +416,7 @@ export function ChatContent() {
         })}
       </div>
 
-      <div className='flex fixed bottom-0 left-0 w-full pl-[414px] px-16 items-center justify-center'>
+      <div className='flex  fixed bottom-0 left-0 w-full pl-[478px] px-16  items-center justify-center'>
      <div className={`p-4 border border-gray-300 group rounded-2xl w-full mb-16 min-h-[100px] flex-col transition-all ${botaoTermosClicado ? 'hover:border-blue-400' : ''} ${botaoLivrosCapitulosClicado ? 'hover:border-pink-400' : ''} ${botaoEventosClicado ? 'hover:border-orange-400' : ''} ${botaoResumoClicado ? 'hover:border-yellow-400' : ''} ${botaoAreasClicado ? 'hover:border-green-400' : ''} ${botaoPesquisadoresClicado ? 'hover:border-red-400' : ''} ${botaoPatentesClicado ? 'group-hover:border-cyan-400' : ''}`}>
      <div className={`flex  items-center h-10  w-full  text-base font-medium  justify-center transition  `}>
                 <MagnifyingGlass size={20} className={`text-gray-400 min-w-[38px] ${botaoTermosClicado ? 'group-hover:text-blue-400' : ''} ${botaoResumoClicado ? 'group-hover:text-yellow-400' : ''} ${botaoLivrosCapitulosClicado ? 'group-hover:text-pink-400' : ''} ${botaoEventosClicado ? 'group-hover:text-orange-400' : ''} ${botaoAreasClicado ? 'group-hover:text-green-400' : ''} ${botaoPesquisadoresClicado ? 'group-hover:text-red-400' : ''} ${botaoPatentesClicado ? 'group-hover:text-cyan-400' : ''}`} />
@@ -316,12 +429,59 @@ export function ChatContent() {
                   id="inputQuestion" className={`flex flex-1 h-full outline-none`} />
 
 <div className='flex items-center justify-center'>
+  
                   <div className={`absolute z[-999] animate-ping gap-4 text-white rounded-xl h-[28px] w-[28px] justify-center hover:bg-blue-500  font-medium transition ${botaoTermosClicado ? 'bg-blue-400' : ''} ${botaoResumoClicado ? 'bg-yellow-400' : ''} ${botaoAreasClicado ? 'bg-green-400' : ''} ${botaoEventosClicado ? 'bg-orange-400' : ''} ${botaoLivrosCapitulosClicado ? 'bg-pink-400' : ''} ${botaoPesquisadoresClicado ? 'bg-red-400' : ''} ${botaoPatentesClicado ? 'bg-cyan-400' : ''}`}></div>
                   <div onClick={handleMaria} className={`cursor-pointer flex z-[999] relative  items-center gap-4 text-white rounded-xl h-[38px] w-[38px] justify-center  font-medium transition ${botaoTermosClicado ? 'bg-blue-400' : ''} ${botaoResumoClicado ? 'bg-yellow-400' : ''} ${botaoAreasClicado ? 'bg-green-400' : ''} ${botaoEventosClicado ? 'bg-orange-400' : ''} ${botaoLivrosCapitulosClicado ? 'bg-pink-400' : ''} ${botaoPesquisadoresClicado ? 'bg-red-400' : ''} ${botaoPatentesClicado ? 'bg-cyan-400' : ''}`}><PaperPlaneTilt size={16} className="text-white" /></div>
                 </div>
               </div>
 
-     
+     <div className='flex mt-2 justify-between items-center w-full'>
+<div></div>
+
+<div className='flex gap-2'>
+{isTab && (
+    <div className='flex gap-2'>
+      <div  className={`outline-none cursor-pointer text-sm rounded-xl text-gray-400 flex items-center  justify-center  gap-2  font-semibold transition ${botaoTermosClicado ? "activeTermos px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickTermos} >
+                  <Quotes size={16} className="" />
+                  {selectedTab === 0 && botaoTermosClicado && <span>Termo</span>}
+                </div>
+
+                <div className={`outline-none cursor-pointer text-sm rounded-xl text-gray-400 flex items-center  justify-center  gap-2  font-semibold transition ${botaoResumoClicado ? "activeResumo px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickResumo} >
+                  <TextAlignLeft size={16} className="" />
+                  {selectedTab === 1 && botaoResumoClicado && <span>Resumo</span>}
+                </div>
+
+                <div className={` whitespace-nowrap outline-none cursor-pointer text-sm text-gray-400 rounded-xl flex items-center gap-2 justify-center font-semibold  transition ${botaoLivrosCapitulosClicado ? "activeLivrosCapitulos px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickLivrosCapitulos} >
+                  <Books size={16} className="" />
+                  {selectedTab === 5 && botaoLivrosCapitulosClicado && <span>Livros e capítulos</span>}
+                </div>
+
+                <div  className={` whitespace-nowrap outline-none cursor-pointer text-sm text-gray-400 rounded-xl flex items-center gap-2 justify-center font-semibold  transition ${botaoEventosClicado ? "activeEventos px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickEventos} >
+                  <Ticket size={16} className="" />
+                  {selectedTab === 6 && botaoEventosClicado && <span>Participação em eventos</span>}
+                </div>
+
+                <div className={`outline-none cursor-pointer text-sm text-gray-400 rounded-xl flex items-center gap-2 justify-center font-semibold  transition ${botaoAreasClicado ? "activeAreas px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickAreas}>
+                  <Lightbulb size={16} className="" />
+                  {selectedTab === 2 && botaoAreasClicado && <span>Áreas</span>}
+                </div>
+
+                <div  className={` outline-none cursor-pointer text-sm text-gray-400 rounded-xl flex items-center gap-2 justify-center font-semibold  transition ${botaoPatentesClicado ? "activePatente px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickPatentes} >
+                  <Copyright size={16} className="" />
+                  {selectedTab === 4 && botaoPatentesClicado && <span>Patente</span>}
+                </div>
+
+                <div  className={` outline-none cursor-pointer text-sm text-gray-400 rounded-xl flex items-center gap-2 justify-center  font-semibold  transition ${botaoPesquisadoresClicado ? "activePesquisadores px-4 py-2" : ('hover:bg-gray-100 w-[38px]')}`} onClick={handleClickPesquisadores} >
+                  <IdentificationCard size={16} className="" />
+                  {selectedTab === 3 && botaoPesquisadoresClicado && <span>Nome</span>}
+                </div>
+    </div>
+  )}
+
+<div onClick={() => handleClickClose()} className={`cursor-pointer flex z-[999] relative  items-center gap-3 text-gray-400  rounded-xl h-[38px] w-[38px] justify-center  font-medium transition ${isTab ? (`bg-gray-100 hover:bg-gray-300`):(`bg-gray-50 hover:bg-gray-100`)}`}>{!isTab ? (<Plus size={16} className="" />):(<X size={16} className="" />)}</div>
+<div  className={`cursor-pointer flex z-[999] relative  items-center gap-3 text-gray-400 hover:bg-gray-100 rounded-xl h-[38px] px-4 justify-center  font-medium transition bg-gray-50`}><Trash size={16} className="" />Limpar</div>
+</div>
+     </div>
      </div>
      </div>
 
